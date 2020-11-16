1a534bd00282c5862952557d5cab5293e29d2f06
yamauchi-shinji <gavetomefor258@gmail.com>
2020-11-16 20:52:21 +0900

    セキュリティ実装

--- app/controllers/blacklist_controller.rb
+++ app/controllers/blacklist_controller.rb
@@ -1,16 +1,18 @@
 class BlacklistController < ApplicationController
   def new
+    if current_user.role_id <= 1
+      redirect_to root_path
+    end
     @blacklist = Blacklist.new
-    @blacklists = Blacklist.all
-  end
-
-  def show
     @blacklists = Blacklist.all
   end
 
   def create
     @blacklist = Blacklist.new(blacklist_params)
     if @blacklist.save
+      redirect_to new_blacklist_path
+    else
+      redirect_to root_path
     end
   end
 
